a8c826055359f35ae13c6281822e24ed1467403b
ˊ潶、昰硪旳註銫調。 <975080391@qq.com>
2018-02-07 20:54:52 +0800

    ..

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -9,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: _ => require.ensure([], () => _(require('@/components/HelloWorld')), 'HelloWorld')
     }
   ]
 })
